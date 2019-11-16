1da0a9bb566964d95847b29d3ed2e1698eb8758f
yam t <yam.tirosh@gmail.com>
2019-11-16 21:59:08 +0200

    Add documentation:

--- object_proxy/proxy.py
+++ object_proxy/proxy.py
@@ -1,17 +1,39 @@
 class Proxy(object):
-    COPY_FROM_TYPE = [
+    """
+    A proxy to an object.
+    Any action performed on the proxy will be done on the original object,
+    and the result will be given back to the user.
+    """
+
+    ATTRIBUTES_TO_COPY_FROM_TYPE = [
         "__name__",
         "__doc__",
     ]
 
     @staticmethod
-    def __copy_from_type(obj, proxy_class):
-        for attr_to_copy in Proxy.COPY_FROM_TYPE:
+    def __set_from_type(obj, proxy_class):
+        """
+        Sets attributes of the class of the proxy object
+        to the appropriate attributes of the class of the original object.
+        :param obj: The original object.
+        :type obj: object
+        :param proxy_class: The class of the proxy object.
+        :type proxy_class: type
+        """
+        for attr_to_copy in Proxy.ATTRIBUTES_TO_COPY_FROM_TYPE:
             attr = getattr(type(obj), attr_to_copy)
             setattr(proxy_class, attr_to_copy, attr)
 
     @staticmethod
     def __set_special_methods(obj, proxy_class):
+        """
+        Sets special `__methods__` of the proxy class
+        to call the appropriate method of the original object.
+        :param obj: The original object.
+        :type obj: object
+        :param proxy_class: The class of the proxy object.
+        :type proxy_class: type
+        """
         for attribute_name, attribute in type(obj).__dict__.items():
             if not callable(getattr(obj, attribute_name)):
                 continue
@@ -24,11 +46,20 @@ class Proxy(object):
             setattr(proxy_class, attribute_name, get_wrapped_attribute(attribute_name))
 
     def __new__(cls, obj):
+        """
+        Create a new proxy object.
+        This dynamically creates a class for the new proxy and sets
+        all the necessary attributes and methods to the class before initializing it.
+        :param obj: The original object to create a proxy to.
+        :type obj: object
+        :return: A new proxy object.
+        :rtype: object
+        """
         class _Proxy(object):
             def __getattr__(self, item):
                 return getattr(obj, item)
 
-        cls.__copy_from_type(obj, _Proxy)
+        cls.__set_from_type(obj, _Proxy)
         cls.__set_special_methods(obj, _Proxy)
 
         return _Proxy()
