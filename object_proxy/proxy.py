4c9796c3f8a8e523a0322be0ba6d7b277e02c2e6
yam t <yam.tirosh@gmail.com>
2019-11-16 21:44:15 +0200

    Separate to methods

--- object_proxy/proxy.py
+++ object_proxy/proxy.py
@@ -1,9 +1,17 @@
 class Proxy(object):
-    def __new__(cls, obj):
-        class Wrapper(object):
-            def __getattr__(self, item):
-                return getattr(obj, item)
+    COPY_FROM_TYPE = [
+        "__name__",
+        "__doc__",
+    ]
 
+    @staticmethod
+    def __copy_from_type(obj, proxy_class):
+        for attr_to_copy in Proxy.COPY_FROM_TYPE:
+            attr = getattr(type(obj), attr_to_copy)
+            setattr(proxy_class, attr_to_copy, attr)
+
+    @staticmethod
+    def __set_special_methods(obj, proxy_class):
         for attribute_name, attribute in type(obj).__dict__.items():
             if not callable(getattr(obj, attribute_name)):
                 continue
@@ -13,6 +21,14 @@ class Proxy(object):
                     return getattr(obj, attr_name)(*args, **kwargs)
                 return wrapped_attribute
 
-            setattr(Wrapper, attribute_name, get_wrapped_attribute(attribute_name))
+            setattr(proxy_class, attribute_name, get_wrapped_attribute(attribute_name))
 
-        return Wrapper()
+    def __new__(cls, obj):
+        class _Proxy(object):
+            def __getattr__(self, item):
+                return getattr(obj, item)
+
+        cls.__copy_from_type(obj, _Proxy)
+        cls.__set_special_methods(obj, _Proxy)
+
+        return _Proxy()
