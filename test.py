eedf2c9d53a7beba851a8b36665ca849a95f153d
yam t <yam.tirosh@gmail.com>
2019-11-22 13:39:54 +0200

    Fix get attribute test and minor naming change

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from object_proxy import Proxy
 
 
 class Foo(object):
-    foo = 3
+    value = 3
 
 
 class TestProxy(TestCase):
@@ -19,9 +19,10 @@ class TestProxy(TestCase):
         self.proxy = Proxy(self.foo)
 
     def test_get_attribute(self):
-        assert self.foo.foo == self.proxy.foo, "Proxy did not get the attribute of the original object"
+        self.foo.value = 6
+        assert self.foo.value == self.proxy.value, "Proxy did not get the attribute of the original object"
 
     def test_set_attribute(self):
         expected = 6
-        self.proxy.foo = expected
-        assert self.foo.foo == expected, "Proxy did not set the attribute of the original object"
+        self.proxy.value = expected
+        assert self.foo.value == expected, "Proxy did not set the attribute of the original object"
