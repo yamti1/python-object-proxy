97fdec144cdb29d7829879c4b11c9c9d251fabe5
yam t <yam.tirosh@gmail.com>
2019-11-22 15:57:04 +0200

    Minor refactoring to `TEstProxy`

--- test.py
+++ test.py
@@ -11,30 +11,24 @@ class Foo(object):
 
 
 class TestProxy(TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestProxy, self).__init__(*args, **kwargs)
-
-        self.foo = None
-        self.proxy = None
-
     def setUp(self):
         self.foo = Foo()
         self.proxy = Proxy(self.foo)
 
     def test_get_attribute(self):
         self.foo.value = 6
-        assert self.foo.value == self.proxy.value, "Proxy did not get the attribute of the original object"
+        self.assertEqual(self.foo.value, self.proxy.value, "Proxy did not get the attribute of the original object")
 
     def test_set_attribute(self):
         expected = 6
         self.proxy.value = expected
-        assert self.foo.value == expected, "Proxy did not set the attribute of the original object"
+        self.assertEqual(self.foo.value, expected, "Proxy did not set the attribute of the original object")
 
     def test_del_attribute(self):
         del self.foo.value
 
-        msg = "Attribute in Proxy was not deleted after deleting it in the original object"
-        assert not hasattr(self.proxy, "value"), msg
+        self.assertFalse(hasattr(self.proxy, "value"),
+                         "Attribute in Proxy was not deleted after deleting it in the original object")
 
     @fudge.test
     def test_method_call(self):
@@ -45,4 +39,4 @@ class TestProxy(TestCase):
 
         result = self.proxy.func(*args, **kwargs)
 
-        assert result == expected, "Call to a function through Proxy returned an unexpected result"
+        self.assertEqual(result, expected, "Call to a function through Proxy returned an unexpected result")
